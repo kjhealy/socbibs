d2e73436133f3a0e32c91b19d1658f7bbd628f86
Kieran Healy <kjhealy@gmail.com>
2013-05-15 13:17:45 -0400

    Temporary workaround for pandoc citations

---
+++ bin/procbib.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+## Sort the file
+cp socbib.bib socbib.bak
+bibtool -r bibtool-rsc/crossref.rsc -i socbib.bib -o socbib.bib
+bibtool -s -i socbib.bib -o socbib.bib
+
+## Make a pandoc-ready copy
+cp socbib-pandoc.bib socbib-pandoc.bak
+cp socbib.bib socbib-pandoc.bib
+perl -pi -e 's/:_//g' socbib-pandoc.bib
+perl -pi -e 's/_//g' socbib-pandoc.bib

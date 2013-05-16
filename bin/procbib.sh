d4ba2328f50233876f119f40431e11e2e5ea329a
Kieran Healy <kjhealy@gmail.com>
2013-05-16 16:16:02 -0400

    Fix the script

--- bin/procbib.sh
+++ bin/procbib.sh
@@ -2,8 +2,8 @@
 
 ## Sort the file
 cp socbib.bib socbib.bak
-bibtool -r bibtool-rsc/crossref.rsc -i socbib.bib -o socbib.bib
 bibtool -s -i socbib.bib -o socbib.bib
+bibtool -r bibtool-rsc/crossref.rsc -i socbib.bib -o socbib.bib
 
 ## Make a pandoc-ready copy
 cp socbib-pandoc.bib socbib-pandoc.bak
